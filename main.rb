171c1c04f309c685891e0b29615e6f1d3c57edbf
Corey Noel <corey.g.noel@gmail.com>
2018-06-14 16:09:39 -0400

    integer binding changes to main

--- main.rb
+++ main.rb
@@ -1,13 +1,21 @@
 require_relative 'map.rb'
+
 if ARGV.length != 2
-  puts "Usage: ruby gold_rush.rb [seed] [num prospectors]"
+ puts "Usage: ruby gold_rush.rb [seed] [num prospectors]"
+ exit 1
+end
+
+begin
+  srand(Integer(ARGV[0]))
+  count = Integer ARGV[1]
+rescue
+  puts "Input values must be integers"
   exit 1
 end
 
-srand ARGV[0].to_i
-
 (1..ARGV[1].to_i).each do |i|
-  mp = Map.new; mp.load_default_map
+  mp = Map.new
+  mp.load_default_map
   puts "Prospector ##{i}'s adventure begins in #{mp.start.city_name}..."
   # game
 end
